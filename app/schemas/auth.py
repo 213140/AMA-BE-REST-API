beaaba584c114bd92f0c4077f5d4c8c44b6758e6
Andžej Mackevič <mackiewicz1995@gmail.com>
2026-01-28 10:22:03 +0100

    Switched hashing from bcrypt to PBKDF2-SHA256, due to password hashing issue on Windows

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, EmailStr, Field
 
 class RegisterRequest(BaseModel):
     email: EmailStr
-    password: str = Field(min_length=6, max_length=128)
+    password: str = Field(min_length=6, max_length=72)
 
 class LoginRequest(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=1, max_length=72)
 
 class TokenResponse(BaseModel):
     access_token: str
